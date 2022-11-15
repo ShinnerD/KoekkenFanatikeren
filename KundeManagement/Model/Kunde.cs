7b33fb953211f40229038d942d1267de79ad3e80
Erik Roest Holst <erho47738@edu.ucl.dk>
2022-11-15 20:22:01 +0100

    Merge pull request #test1 from ShinnerD/Implement-Repository
    Har implmenteret kunderepository.

--- KundeManagement/Model/Kunde.cs
+++ KundeManagement/Model/Kunde.cs
@@ -9,6 +9,9 @@ namespace DataHandling.Model
     internal class Kunde
     {
         public int KundeId { get; set; }
-
+        public int KundeAdresse { get; set; }
+        public string KundeName { get;}
+        public int KundeInfo { get; set; }
+        public int test { get; set; }
     }
 }
