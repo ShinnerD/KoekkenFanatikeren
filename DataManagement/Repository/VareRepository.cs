3e413a18af488911ae80d84c0d6880d04b61eb7e
Jonas Haydar Qasem <jhqa49472@edu.ucl.dk>
2022-11-27 14:59:22 +0100

    Har tilføje kode til Model,Repository og Service: Vare og Varegruppe

--- DataManagement/Repository/VareRepository.cs
+++ DataManagement/Repository/VareRepository.cs
@@ -1,6 +1,55 @@
-﻿namespace KundeManagement.Repository
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataManagement.Database;
+using KundeManagement.Model;
+
+namespace KundeManagement.Repository
 {
+  
+
     public class VareRepository
     {
+        public KFDataClassesDataContext DataContext { get; set; }
+
+        public VareRepository()
+        {
+            DataContext = new KFDataClassesDataContext();
+        }
+
+        public List<Vare> GetAllVare()
+        {
+            List<Vare> result = new List<Vare>();
+
+            var VareIdListe = DataContext.Products;
+
+            foreach (var vare in VareIdListe)
+            {
+                result.Add(GetOneVare(vare.Product_Id));
+            }
+            return result;
+        }
+
+        public Vare GetOneVare(int Id)
+        {
+            var dtovare = DataContext.Products.FirstOrDefault(i => i.Product_Id == Id);
+
+            Vare result = new Vare();
+            result.Product_ID = dtovare.Product_Id;
+            result.ProductGroup_ID = dtovare.ProductGroup_Id;
+            result.ProductName = dtovare.ProductName;
+            result.Price = dtovare.Price;
+            result.Material = dtovare.Material;
+            result.Colour = dtovare.Colour;
+            result.Grip = dtovare.Grip;
+
+            VareGruppeRepository vareGruppeRepository = new VareGruppeRepository();
+            result.Varegruppe = vareGruppeRepository.FindEnVaregruppe(dtovare.ProductGroup_Id);
+
+            return result;
+        }
+       
     }
 }
--- DataManagement/Service/VareService.cs
+++ DataManagement/Service/VareService.cs
@@ -1,6 +1,18 @@
-﻿namespace KundeManagement.Service
+﻿using KundeManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KundeManagement.Service
 {
     public class VareService
     {
+        public List<Vare> RetunererAlleVarerService()
+        {
+            var VareRepository = new Repository.VareRepository();
+            return VareRepository.GetAllVare();
+        }
     }
 }
