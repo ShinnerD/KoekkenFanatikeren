ead3f0a3229e08f6baf3690b8139e368d9fc2ec3
Dennis <skyggendk@gmail.com>
2022-12-06 18:53:16 +0100

    lille fejlkorrektion

--- DataManagement/Service/KundeService.cs
+++ DataManagement/Service/KundeService.cs
@@ -15,26 +15,10 @@ namespace DataManagement.Service
             return new Repository.KundeRepository().GetAllKunder();
         }
 
-
-
-
         public Kunde GetKunde(int kundeId)
         {
             return new Repository.KundeRepository().GetKunde(kundeId);
         }
-
-        public void SaveNewKunde(Kunde kunde)
-        {
-            new Repository.KundeRepository().SaveNewKunde( kunde);
-        }
-        public void EditKunde(Kunde kunde)
-        {
-             new Repository.KundeRepository().EditKunde(kunde);
-        }
-        public void DeleteKunde(Kunde kunde)
-        {
-             new Repository.KundeRepository().DeleteKunde(kunde);
-
 
         public void SaveNewKunde(Kunde kunde)
         {
@@ -49,7 +33,6 @@ namespace DataManagement.Service
         public void DeleteKunde(Kunde kunde)
         {
             new Repository.KundeRepository().DeleteKunde(kunde);
-
         }
     }
 }
